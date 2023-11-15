1fd15f1572205af5e97ef98713515c92bcb00ad3
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2023-11-15 14:00:22 -0500

    SupermarketEF

---
+++ Data/SumermarketContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketEF.Data
+{
+    internal class SumermarketContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=SupermarketEF;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketEF.Models
+{
+    public class Product
+    {
+        // [KEY] -> Anotacion si la propiedad no se llama Id, ejemplo ProductId
+        public int Id { get; set; } // Sera la llave primaria
+        public string Name { get; set; }
+        [Column(TypeName ="decimal(6,2")]
+        public decimal Price { get; set; }
+        public int Stock {  get; set; }
+        public int CategoryId { get; set; } // Sera la llave foreanea 
+        public Category Category { get; set; } // Propiedad de navegacion
+
+    }
+}
